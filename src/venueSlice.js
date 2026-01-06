5dff8d902fc1957ae9ccac0dee72f81ff9aa40d9
aubin-pierre7 <aubinp58@gmail.com>
2026-01-06 19:27:56 +0100

    Quelques modifications

--- src/venueSlice.js
+++ src/venueSlice.js
@@ -1,36 +1,35 @@
 // venueSlice.js
 import { createSlice } from "@reduxjs/toolkit";
-
 export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
-      name: "Conference Room (Capacity:15)",
+      img: "",
+      name: "Salle de conférence (Capacité:15)",
       cost: 3500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/event-venue-1597531_640.jpg",
-      name: "Auditorium Hall (Capacity:200)",
+      img: "",
+      name: "Salle d'auditorium (Capacité:200)",
       cost: 5500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/convention-center-3908238_640.jpg",
-      name: "Presentation Room (Capacity:50)",
+      img: "",
+      name: "Salle de présentation (Capacité:50)",
       cost: 700,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
-      name: "Large Meeting Room (Capacity:10)",
+      img: "",
+      name: "Grande salle de réunion (Capacité:10)",
       cost: 900,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/laptops-593296_640.jpg",
-      name: "Small Meeting Room (Capacity:5)",
+      img: "",
+      name: "Petite salle de réunion (Capacité:5)",
       cost: 1100,
       quantity: 0,
     },
@@ -41,7 +40,7 @@ export const venueSlice = createSlice({
     incrementQuantity: (state, action) => {
       const { payload: index } = action;
       if (state[index]) {
-        if (state[index].name === " Auditorium Hall (Capacity:200)" && state[index].quantity >= 3) {
+        if (state[index].name === " Salle d'auditorium (Capacité:200)" && state[index].quantity >= 3) {
           return;        }
         state[index].quantity++;
       }
@@ -54,7 +53,5 @@ export const venueSlice = createSlice({
     },
   },
 });
-
 export const { incrementQuantity, decrementQuantity } = venueSlice.actions;
-
 export default venueSlice.reducer;
